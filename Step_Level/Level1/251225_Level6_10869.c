99689f3415682bbff3b1735661c6971aca085f89
WinOneee <https://github.com/WinOneee/Beakjooon>
2025-12-25 18:42:37 +0900

    예원아 사랑해

---
+++ Step_Level/Level1/251225_Level6_10869.c
@@ -0,0 +1,19 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int A, B;
+
+	scanf("%d", &A);
+	scanf("%d", &B);
+
+	printf("%d\n", A + B);
+	printf("%d\n", A - B);
+	printf("%d\n", A * B);
+	printf("%d\n", A / B);
+	printf("%d\n", A % B);
+
+
+	return 0;
+}
---
+++ Step_Level/Level1/251225_Level8_18108.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int A;
+    int B = 543;
+
+    scanf("%d", &A);
+
+    printf("%d", A-B);
+
+    return 0;
+}
