47918e41a7b03d659eada3f3740ace073e5d9715
WinOneee <swjeong9280@gmail.com>
2025-12-27 09:47:13 +0900

    251227 업로드

---
+++ Step_Level/Level1/251227_Level10_2588.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int A, B;
+
+	scanf("%d %d", &A, &B);
+
+	printf("%d\n", A * (B % 10)); //(3)
+	printf("%d\n", A * ((B % 100) / 10)); //(4)
+	printf("%d\n", A * (B / 100)); //(5)
+	printf("%d\n", A * B); //(6)
+
+	return 0;
+}
