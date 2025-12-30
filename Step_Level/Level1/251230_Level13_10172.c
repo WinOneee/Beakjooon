85aac9ab328c6fb6f3c39bb4b05d449b12def586
WinOneee <swjeong9280@gmail.com>
2025-12-30 09:22:41 +0900

    251230 업로드

---
+++ Step_Level/Level1/251230_Level13_10172.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|\n");
+
+	// " 그냥 쓸경우 끝났다고 판단하여 앞에 \ 사용해야함. 
+	// 또한 \ 사용 필요시 \\ 두번 사용하여야 함.
+
+    return 0;
+}
